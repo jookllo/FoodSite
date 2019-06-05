13528c76fa92ac62b9ca19c8b07003f099a0d756
jookllo <hokello34@gmail.com>
2019-06-05 23:17:38 +0300

    the band, index and menu have been added and updated. The about page will be done in a bit.

---
+++ menu.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Menu</title>
+  </head>
+  <body>
+    <?php include_once "header.php";?>
+    <div class="body">
+    <h2>The Casual Menu</h2>
+          <p>The restaurant offers a variation of meals to eat for customers ranging from meat based diets, vegeterian diets and also quick snacks to eat and enjoy. The people can choose what they want to eat and suggestions can be made in house.</p>
+          <p>The special meals are also there for you to choose from and it's in the daily special meals. The people can order from the various items we provide:</p>
+          <div class="table">
+          <table>
+            <tr>
+                <th>DAY</th>
+                <th>Breakfast</th>
+                <th>Lunch</th>
+                <th>Supper</th>
+            </tr>
+            <tr>
+              <th>Monday</th>
+              <td>Tea/Coffee,toast,eggs and sausage</td>
+              <td>Fried rice,chicken tika,chapati,salad</td>
+              <td>Spaghetti,meatballs </td>
+            </tr>
+            <tr>
+              <th>Tuesday</th>
+              <td>Tea/Coffee,baked beans,pancakes and bacon</td>
+              <td>Pilau,cow peas,kales</td>
+              <td>Beef and rice </td>
+            </tr> 
+            <tr>
+              <th>Wednesday</th>
+              <td>Tea/Coffee,grilled cheese sandwich and sausage</td>
+              <td>Ugali and Spinach</td>
+              <td>Baked potatoes, lamb soup and bread</td>
+            </tr>
+            <tr>
+              <th>Thursday</th>
+              <td>Tea/Coffee and arrow roots/sweet potatoes</td>
+              <td>Salad and any type of meat available</td>
+              <td>Rice/Ugali, chicken with Kale </td>
+            </tr>
+            <tr>
+              <th>Friday</th>
+              <td span="">Request Fridays</td>
+            </tr>   
+          </table>
+        </div>
+          <p>The meals on the menu when requested will cost the <strong>Same Price</strong> each day.</p>
+        </div>  
+    <?php include_once "footer.php";?>
+  </body>
+</html>
