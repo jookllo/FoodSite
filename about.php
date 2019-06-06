52a2a0ded02e8fcbcc14d90a9726438bb72dcbb1
jookllo <hokello34@gmail.com>
2019-06-06 10:12:48 +0300

    Added details to the page

--- about.php
+++ about.php
@@ -5,6 +5,14 @@
 	</head>
 	<body>
 		<?php include_once "header.php";?>
+		<div class="body">
+			<h2>Our Mission</h2>
+				<p>The restaurant that allows an all round entertainment spot for the whole family to enjoy and laugh toghether. we strive to provide the best service for the city of Nairobi.</p>
+			<h2>Our Vision</h2>
+				<p>To grow within and also around Kenya and improve our services. We would want to cater to all types of lifestyles and also groups of various dietary types.Making us improve our services beyond the ones we currently offer.</p>
+			<h2>Our Goal</h2>
+				<p>Produce food grown in our own gardens to supply the whole of Kenya and its neighbours. This is to allow for quick supply to the restaurants and also speed up the rate of operations in our farms. With this we can provide food to all types of people.</p>
+		</div>
 		<?php include_once "footer.php";?>
 	</body>
 </html>
