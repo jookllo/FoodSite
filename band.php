9182dd35ce9ed8ce0e7166f683af9d72891e374a
jookllo <hokello34@gmail.com>
2019-06-14 13:00:40 +0300

    all the header deatils have been put into individual files

--- band.php
+++ band.php
@@ -22,7 +22,27 @@
 </style>
   </head>
   <body>
-    <?php include_once "header.php";?>
+  <div class="header">
+  <style media="screen" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </style>
+  
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+    <h1 class="name">Mamachakula</h1>
+    <br>
+    <div class="navbar">
+      <ul class="nav-list">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="menu.php">Menu</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="band.php">Band</a></li>
+      </ul>
+    </div>
+  </div>
+
     <div class="body">
       <div class="bdy">
         <br>
@@ -61,8 +81,11 @@
 
 
                 <h3>Let us hear who you'd like us to call in to present in our Band Play Sunday's.</h3>
-                <input type="text" name="band">
-                <button type="button">Submit</button>
+                <form>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
 </div>
 
 </div>
