25e3f67c3ce5d805994b1c9626a5db06ce7719f1
Okello Joey <jookllo32@gmail.com>
2022-08-24 00:52:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>MamaChakula</title>
+        <title>Mama Chakula</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Site Description Here">
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
